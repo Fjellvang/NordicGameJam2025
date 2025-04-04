46fb2536330ffa3e1efac88f20d562ba5174ea5e
Aleksander <aleksander@fjellvang.dk>
2025-04-04 20:59:08 +0200

    player prefab

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DotsShooter
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        /// <summary>
+        /// The Singleton instance
+        /// </summary>
+        public static T Instance => _instance;
+
+        public virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }}
