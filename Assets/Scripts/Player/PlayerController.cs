65ab392d502dc1fea0f7aa323a13af6f926263a3
Dansk Torskerovnsforening <59346726+Ierdyr@users.noreply.github.com>
2025-04-04 21:35:44 +0200

    Made basic player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,8 +2,12 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    Rigidbody rig;
+    Vector2 input;
     InputActions inputActions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnEnable()
@@ -11,15 +15,31 @@ public class PlayerController : MonoBehaviour
         inputActions = new InputActions();
         inputActions.Player.Enable();
         inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
     }
 
     private void OnMove(InputAction.CallbackContext obj)
     {
+        input = obj.ReadValue<Vector2>();
     }
-    
+
+    private void Update()
+    {
+        var dir = new Vector3(input.x * speed, rig.linearVelocity.y, input.y * speed);
+        rig.linearVelocity = dir;
+    }
+
     private void OnDisable()
     {
+
+
         inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
         inputActions.Player.Disable();
     }
 }
