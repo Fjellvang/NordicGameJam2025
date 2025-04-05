c3928fe5b43facc02801e48246f43017f0b3c554
Aleksander <aleksander@fjellvang.dk>
2025-04-05 12:51:27 +0200

    add some quick sound

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioMixerGroup insideaudioMixerGroup;
+    public AudioMixerGroup outsideaudioMixerGroup;
+    
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    [ContextMenu("Play Audio Outside")]
+    public void PlayAudioOutside()
+    {
+        audioSource.outputAudioMixerGroup = outsideaudioMixerGroup;
+    }
+
+    [ContextMenu("Play Audio Inside")]
+    public void PlayAudioInside()
+    {
+        audioSource.outputAudioMixerGroup = insideaudioMixerGroup;
+    }
+}
