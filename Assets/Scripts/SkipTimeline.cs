d12297a652d0a36ed0c5ec488de93ae11e551a16
Aleksander <aleksander@fjellvang.dk>
2025-04-06 10:06:51 +0200

    timeline skip

---
+++ Assets/Scripts/SkipTimeline.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class SkipTimeline : MonoBehaviour
+{
+    
+    PlayableDirector playableDirector;
+    private void Awake()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playableDirector.state == PlayState.Playing)
+        {
+            playableDirector.time = playableDirector.duration;
+            playableDirector.Evaluate();
+            playableDirector.Stop();
+        }
+    }
+}
